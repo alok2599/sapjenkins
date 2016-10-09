e1d3715d2433bbdab691de5985a5c84a4352db65
alok2599 <alok2599@gmail.com>
2016-10-09 14:23:46 +0530

    hello

--- sapjenkins/src/main/java/sapjenkins/Test.java
+++ sapjenkins/src/main/java/sapjenkins/Test.java
@@ -3,8 +3,8 @@ package sapjenkins;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
-		//my change
+		System.out.println("hello");
+		//my chang
 
 	}
 
